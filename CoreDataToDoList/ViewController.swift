e6fc040f6b17f34861264a6b28e8380c5026adc7
juanmeza <juanmeza255@hotmail.com>
2019-09-09 15:55:30 -0700

    Example about CoreData

--- CoreDataToDoList/ViewController.swift
+++ CoreDataToDoList/ViewController.swift
@@ -7,14 +7,102 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    //var items: [String] = []
+    var items: [NSManagedObject] = []
+    
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        
+        do {
+            items = try managedContext.fetch(fetchRequest)
+        } catch let err as NSError {
+            print("Failed to fetch item", err)
+        }
+        
+    }
+    
+    
+    @objc func addItem(_ sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Add new item", message: "Please fill in the textfield below", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            guard let textField = alertController.textFields?.first, let itemToAdd = textField.text else { return }
+            
+            self.save(itemToAdd)
+            //self.items.append(itemToAdd)
+            self.tableView.reloadData()
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func save(_ itemName: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Item", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(itemName, forKey: "itemName")
+        
+        do {
+            
+            try managedContext.save()
+            items.append(item)
+            
+        } catch let err as NSError {
+            print("Failed to save as item")
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let item = items[indexPath.row]
+        
+        cell.textLabel?.text = item.value(forKeyPath: "itemName") as? String
+        //cell.textLabel?.text = item
+        
+        return cell
+    }
+    
+   
 }
 
